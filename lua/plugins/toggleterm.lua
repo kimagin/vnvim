404f08603516555897f1507f4e728703972a1bc7
kimagin <info@kimagination.net>
2023-12-13 02:21:50 +0100

    fix: terminal switches directory

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,21 +1,23 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  opts = {--[[ things you want to change go here]]
-    --   size = vim.o.columns * 0.4,
-    open_mapping = [[<c-\>]],
-    --   shade_terminals = false,
-    --   start_in_insert = true,
-    --   terminal_mappings = false,
-    --   insert_mappings = true,
-    --   autochdir = true,
-    --   persist_size = true,
-    --   persist_mode = true,
-    --   close_on_exit = true,
-    --   direction = "vertical",
-    --   auto_scroll = true,
-    float_opts = {
-      border = "curved",
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {--[[ things you want to change go here]]
+      size = vim.o.columns * 0.1,
+      open_mapping = [[<c-\>]],
+      shade_terminals = false,
+      start_in_insert = true,
+      terminal_mappings = true,
+      insert_mappings = true,
+      autochdir = true,
+      persist_size = true,
+      persist_mode = false,
+      close_on_exit = true,
+      direction = "horizontal",
+      auto_scroll = true,
+      float_opts = {
+        border = "border",
+      },
     },
   },
 }
