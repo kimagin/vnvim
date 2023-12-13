e6cef3b8b3bfadfb14cdd4cbcc983e5d1caea67e
kimagin <info@kimagination.net>
2023-12-13 02:20:30 +0100

    project manager now changes the directory

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,20 +1,11 @@
 return {
   "ahmedkhalf/project.nvim",
-  opts = {
-    manual_mode = false,
-    detection_methods = { "pattern" },
-    patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
-    -- silent_chdir = false,
-    scope_chdir = "global",
-  },
-  event = "VeryLazy",
-  config = function(_, opts)
-    require("project_nvim").setup(opts)
-    require("lazyvim.util").on_load("telescope.nvim", function()
-      require("telescope").load_extension("projects")
-    end)
+  lazy = false,
+  config = function()
+    require("project_nvim").setup({
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    })
   end,
-  keys = {
-    { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
-  },
 }
