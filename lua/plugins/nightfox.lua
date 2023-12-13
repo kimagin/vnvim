caa9668101e2ca7aaae8e066c78f390e19629fb6
kimagin <info@kimagination.net>
2023-12-13 02:19:47 +0100

    modifying the theme for new os

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -5,49 +5,15 @@ return {
     event = "VeryLazy",
     opts = {
       options = {
+        fox = "nordfox",
         transparent = true,
-        styles = {
-          comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
-          conditionals = "NONE",
-          constants = "NONE",
-          functions = "NONE",
-          keywords = "NONE",
-          numbers = "NONE",
-          operators = "NONE",
-          strings = "NONE",
-          types = "NONE",
-          variables = "NONE",
-        },
-        groups = {
-          TelescopeBorder = { fg = "#364962" },
-          TelescopeSelection = { bg = "#1e2932" },
-          LazyNormal = { bg = "#11111b" },
-          WhichKeyFloat = { bg = "#11111b" },
-          NotifyWarnBorder = { fg = "#c29452" },
-          NotifyWARNTitle = { fg = "#c29452" },
-          NotifyWARNIcon = { fg = "#c29452" },
-          NotifyWARNTitle = { fg = "#c29452" },
-          NoiceCmdline = { bg = "#c29452" },
-          -- DashboardHeader = { fg = "#5a90a2" },
-          DashboardFooter = { fg = "#81b29a" },
+        terminal_colors = true,
+        inverse = {
+          match_paren = false,
+          visual = true,
+          search = true,
         },
       },
-      -- fox = "nordfox",
-      -- transparent = true,
-      -- terminal_colors = true,
-      -- styles = {
-      --
-      --   sidebars = "transparent",
-      --   floats = "transparent",
-      -- },
-      -- colors = {
-      --   red = "#145000",
-      -- },
     },
-    -- groups = {
-    --   nordfox = {
-    --     LazyNormal = { bg = "#11111b" },
-    --   },
-    -- },
   },
 }
